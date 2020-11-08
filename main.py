2eea042842fedc84f40d0f91b99eef34f0ae5005
Szymon <73613823+SzymonSikora21@users.noreply.github.com>
2020-11-08 16:22:43 +0100

    Change names

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ shop_list = {
 for shop, products in shop_list.items():
     print(f"Idę do {shop} i kupuję tam {products}")
 
-X = len(shop_list.get("Piekarnia")) + len(shop_list.get("Warzywniak"))
-print(f"W sumie kupuję {X} produktów.")
+products_amount = len(shop_list.get("Piekarnia")) + len(shop_list.get("Warzywniak"))
+print(f"W sumie kupuję {products_amount} produktów.")
